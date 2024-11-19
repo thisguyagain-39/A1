0380d125cd3a14bbf081fa675cdc038906763d94
thisguyagain-39 <cframpto@confederationcollege.ca>
2024-11-19 09:23:17 -0500

    aaaaaaaaa

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+module.exports = function(eleventyConfig) {
+
+     eleventyConfig.addWatchTarget("./_src/sass/");
+
+    return {
+         dir: {
+              input: "_src"
+         }
+    }
+}
